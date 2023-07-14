8994b052c7101c3fd776e50cc509ee3e1b367f3f
edisongom <edison.gomez.am@gmail.com>
2023-07-13 22:35:04 -0400

    Modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavbarHeader from "./Components/Navbar";
-import Home from "./Pages/Home";
-import Contacto from "./Pages/Contact";
-import NotFound from "./Pages/NotFound";
+import Home from "./Views/Home";
+import Contacto from "./Views/Contact";
+import NotFound from "./Views/NotFound";
 import Footer from "./Components/Footer";
 
 function App() {
